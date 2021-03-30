8d0be436041425b371c8d64d2ca15bf6317a82e7
Adyson Bruno <adysonbruno@gmail.com>
2021-03-30 11:26:46 -0300

    add logic gaming test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,45 @@
 const createTowers = ()=>{
     let main = document.getElementById("main");
 
+    let cardStart = document.createElement("div")
+    cardStart.className = "card"
+
     let start = document.createElement("div");
     start.className = "towers";
     start.id = "start";
-    main.appendChild(start);
+    cardStart.appendChild(start)
+    let h2Start = document.createElement("h2")
+    h2Start.innerText = "Start"
+    cardStart.appendChild(h2Start)
+
+    main.appendChild(cardStart);
+
+
+    let cardOffset = document.createElement("div")
+    cardOffset.className = "card"
 
     let offset = document.createElement("div");
     offset.className = "towers";
     offset.id = "offset";
-    main.appendChild(offset);
+    cardOffset.appendChild(offset)
+    let h2Offset = document.createElement("h2")
+    h2Offset.innerText = "Offset"
+    cardOffset.appendChild(h2Offset)
+
+    main.appendChild(cardOffset);
+
+    let cardEnd = document.createElement("div")
+    cardEnd.className = "card"
 
     let end = document.createElement("div");
     end.className = "towers";
     end.id = "end";
-    main.appendChild(end)
+    cardEnd.appendChild(end)
+    let h2End = document.createElement("h2")
+    h2End.innerText = "End"
+    cardEnd.appendChild(h2End)
+
+    main.appendChild(cardEnd);
 }
 
 createTowers();
@@ -44,3 +69,89 @@ const createDisc = () =>{
 }
 
 createDisc();
+
+
+
+//
+// let first;
+// let second;
+// let lastElementFirst;
+// let lastElementSecond;
+// let count =0;
+// const h2Jogada = document.getElementById("jogada");
+//
+// const start = document.getElementById("start")
+// start.addEventListener('click', event => {
+//     if(count===0){
+//         first = event.currentTarget.id;
+//         lastElementFirst = start.lastElementChild;
+//         count++;
+//         h2Jogada.innerText = "Para onde quer mover?";
+//     }else if(count===1){
+//         second = event.currentTarget.id;
+//         if(start.lastElementChild!==null){
+//             lastElementSecond = start.lastElementChild;
+//         }
+//
+//         if(start.lastElementChild===null || lastElementFirst.clientWidth<lastElementSecond.clientWidth){
+//             let discMove = document.getElementById(lastElementFirst.id);
+//             start.appendChild(discMove);
+//             h2Jogada.innerText = "Escolha um bloco para mover:";
+//         }else{
+//             alert("Não é possível colocar um bloco maior em cima de um menor!");
+//         }
+//         count=0;
+//     }
+//
+// });
+//
+// const offset = document.getElementById("offset")
+// offset.addEventListener('click', event => {
+//     if(count===0){
+//         first = event.currentTarget.id;
+//         lastElementFirst = offset.lastElementChild;
+//         count++;
+//         h2Jogada.innerText = "Para onde quer mover?";
+//     }else if(count===1){
+//         second = event.currentTarget.id;
+//         if(offset.lastElementChild!==null){
+//             lastElementSecond = offset.lastElementChild;
+//         }
+//         if(offset.lastElementChild===null || lastElementFirst.clientWidth<lastElementSecond.clientWidth){
+//             let discMove = document.getElementById(lastElementFirst.id);
+//             offset.appendChild(discMove);
+//             h2Jogada.innerText = "Escolha um bloco para mover:";
+//         }else{
+//             alert("Não é possível colocar um bloco maior em cima de um menor!");
+//         }
+//         count=0;
+//     }
+// });
+//
+// const end = document.getElementById("end")
+// end.addEventListener('click', event => {
+//     if(count===0){
+//         first = event.currentTarget.id;
+//         lastElementFirst = end.lastElementChild;
+//         count++;
+//         h2Jogada.innerText = "Para onde quer mover?";
+//     }else if(count===1){
+//         second = event.currentTarget.id;
+//         if(end.lastElementChild!==null){
+//             lastElementSecond = end.lastElementChild;
+//         }
+//         if(end.lastElementChild===null || lastElementFirst.clientWidth<lastElementSecond.clientWidth){
+//             let discMove = document.getElementById(lastElementFirst.id);
+//             end.appendChild(discMove);
+//             h2Jogada.innerText = "Escolha um bloco para mover:";
+//         }else{
+//             alert("Não é possível colocar um bloco maior em cima de um menor!");
+//         }
+//         count=0;
+//     }
+//
+//     if(end.childElementCount===4){
+//         alert("VOCÊ VENCEU!!!! PARABÉNS!!!")
+//     }
+// });
+//
